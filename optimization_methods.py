ab186a246bb05b45d0285f70dc685b0831e4489a
Pedro Gabriel Gengo Lourenço <pedro.gabriel.lourenco@hotmail.com>
2019-10-03 21:22:35 -0300

    Add files via upload
    Inicio da classe

---
+++ optimization_methods.py
@@ -0,0 +1,10 @@
+class optimization_methods():
+	def __init__(self, objective_function, gradient, hessian = None):
+
+		self.o_function = objective_function
+		self.gradient = gradient
+		self.hessian = hessian
+
+	def gradient_method(self, x_inicial, tol, ...):
+
+	def 
